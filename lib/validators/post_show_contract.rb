1d780b205b137bb710dc582861019da3fc6801e5
pavelgyravel <pavelgyravel@gmail.com>
2020-08-26 08:21:20 +0300

    Fix validation

--- lib/validators/post_show_contract.rb
+++ lib/validators/post_show_contract.rb
@@ -1,7 +1,7 @@
 module Validators
   class PostShowContract < BaseContract
     params do
-      required(:id).value(:integer, gt?: 0, lteq?: 1000)
+      required(:id).value(:integer, gt?: 0)
     end
   end
 end
